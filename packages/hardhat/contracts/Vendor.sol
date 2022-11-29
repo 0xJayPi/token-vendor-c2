9c66c7d97987ca54760bc6c112f399c89f386971
0xJayPi <jpcampaya@gmail.com>
2022-11-29 09:20:16 +0100

    Challenge 3 completed

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,23 +1,58 @@
-pragma solidity 0.8.4;  //Do not change the solidity version as it negativly impacts submission grading
+pragma solidity 0.8.4; //Do not change the solidity version as it negativly impacts submission grading
 // SPDX-License-Identifier: MIT
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
+error Vendor__TransferFailed();
+
 contract Vendor is Ownable {
+    uint256 public constant tokensPerEth = 100;
 
-  //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+    event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+    event SellTokens(
+        address seller,
+        uint256 amountOfETH,
+        uint256 amountOfTokens
+    );
 
-  YourToken public yourToken;
+    YourToken public yourToken;
 
-  constructor(address tokenAddress) {
-    yourToken = YourToken(tokenAddress);
-  }
+    constructor(address tokenAddress) {
+        yourToken = YourToken(tokenAddress);
+    }
 
-  // ToDo: create a payable buyTokens() function:
+    function buyTokens() public payable {
+        uint256 _amountToTransfer;
+        _amountToTransfer = tokensPerEth * msg.value;
 
-  // ToDo: create a withdraw() function that lets the owner withdraw ETH
+        yourToken.transfer(msg.sender, _amountToTransfer);
 
-  // ToDo: create a sellTokens(uint256 _amount) function:
+        emit BuyTokens(msg.sender, msg.value, _amountToTransfer);
+    }
 
+    function withdraw() public payable onlyOwner {
+        (bool success, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        if (!success) {
+            revert Vendor__TransferFailed();
+        }
+    }
+
+    function sellTokens(uint256 _amount) public payable {
+        uint256 _amountToTransfer;
+        _amountToTransfer = _amount / tokensPerEth;
+
+        yourToken.transferFrom(msg.sender, address(this), _amount);
+
+        (bool success, ) = payable(msg.sender).call{value: _amountToTransfer}(
+            ""
+        );
+        if (!success) {
+            revert Vendor__TransferFailed();
+        }
+
+        emit SellTokens(msg.sender, msg.value, _amountToTransfer);
+    }
 }
